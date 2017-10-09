9661506b60bdfa487bae86b4796f49cb5bf82bdc
Victor Peralta <peralta.r.victor@gmail.com>
2017-10-09 08:54:47 -0700

    Modified for class-less functioning

--- jsonOrder.py
+++ jsonOrder.py
@@ -1,5 +1,5 @@
 import json
-from Orden import *
+import statistics
 
 FILENAME = "orders.json"
 jsonSampleString = '{"datetime": "2017-01-01 23:23:23", "request_id": "123-123-123", "orden": [{"part_id": "123-111","type": "taco","meat": "asada","quantity": 3,"ingredients": ["cebolla","salsa"]},{"part_id": "123-222","type": "mulita","meat": "asada","quantity": 1,"ingredients": [  ]}]}'
@@ -8,25 +8,13 @@ jsonSampleString = '{"datetime": "2017-01-01 23:23:23", "request_id": "123-123-1
 ## Function that converts a json string to an Orden object
 ##
 
-def crearOrden(jsonInputString):
+def parseJSON(jsonInputString):
     jsonObject = json.loads(jsonInputString)
-    listaOrdenes = jsonObject["orden"]
-    listaSubordenes = []
-    for orden in listaOrdenes:
-        s = subOrden(orden["part_id"], orden["type"],orden["meat"], orden["quantity"], orden["ingredients"])
-        listaSubordenes.append(s)
-    return Orden(jsonObject["datetime"], jsonObject["request_id"],listaSubordenes)
+    return jsonObject
+
 
 ##
-## Function that converts an Orden object to a json string (Under develompent)
-##
-
-def serializeOrder(ordenObject):
-    pass
-
-##
-## Function that reads all lines of a text file, traducing them to json strings
-## and creating Orden objects
+## Function that reads all lines of a text file, converting them to json strings
 ##
 def readOrdersFile(fileName):
     with open(fileName, 'r') as f:
@@ -34,11 +22,15 @@ def readOrdersFile(fileName):
         while True:
             line = f.readline()
             if line != '':
-                newOrder = crearOrden(line[:-1]) #Remove breaklines
+                newOrder = parseJSON(line[:-1]) #Remove breaklines
                 listaOrdenes.append(newOrder)
             else:
                 break
         return listaOrdenes
 
 ###MAIN#####
-listaOrdenes = readOrdersFile(FILENAME)
+requestList = readOrdersFile(FILENAME)
+
+
+statistics.printTable(requestList)
+
--- statistics.py
+++ statistics.py
@@ -4,8 +4,8 @@ def printTable(orderList):
     orders = []
     for order in orderList:
         quantity = 0
-        for suborder in order.subOrdenes:
-            quantity += suborder.quantity
-        orders.append([order.request_id, quantity])
+        for suborder in order["orden"]:
+            quantity += suborder["quantity"]
+        orders.append([order["request_id"], quantity])
 
     print(tabulate.tabulate(orders, headers=["Request ID", "Quantity"]))
