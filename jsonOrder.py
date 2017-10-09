68bdd198b619b8fad900fb90fbe8f3a1f6cd2a0c
Fernando Raul Cortez Chavez <fernandoraulcc@gmail.com>
2017-10-08 19:17:56 -0700

    Delete unnecesary use of os module

--- jsonOrder.py
+++ jsonOrder.py
@@ -1,8 +1,7 @@
 import json
-import os
 from Orden import *
 
-FILENAME = os.getcwd() + "\\orders.json"
+FILENAME = "orders.json"
 jsonSampleString = '{"datetime": "2017-01-01 23:23:23", "request_id": "123-123-123", "orden": [{"part_id": "123-111","type": "taco","meat": "asada","quantity": 3,"ingredients": ["cebolla","salsa"]},{"part_id": "123-222","type": "mulita","meat": "asada","quantity": 1,"ingredients": [  ]}]}'
 
 ##
