bc6cb940759cbd56ff8e26790104e3f962eab191
Fernando Raul Cortez Chavez <fernandoraulcc@gmail.com>
2017-10-09 13:15:28 -0700

    Correct some comments

--- jsonOrder.py
+++ jsonOrder.py
@@ -5,7 +5,7 @@ FILENAME = "orders.json"
 jsonSampleString = '{"datetime": "2017-01-01 23:23:23", "request_id": "123-123-123", "orden": [{"part_id": "123-111","type": "taco","meat": "asada","quantity": 3,"ingredients": ["cebolla","salsa"]},{"part_id": "123-222","type": "mulita","meat": "asada","quantity": 1,"ingredients": [  ]}]}'
 
 ##
-## Function that converts a json string to an Orden object
+## Function that converts a json string to an Orden json Object
 ##
 
 def parseJSON(jsonInputString):
@@ -14,7 +14,7 @@ def parseJSON(jsonInputString):
 
 
 ##
-## Function that reads all lines of a text file, converting them to json strings
+## Function that reads all lines of a text file, converting them to json objects (dictionaries)
 ##
 def readOrdersFile(fileName):
     with open(fileName, 'r') as f:
