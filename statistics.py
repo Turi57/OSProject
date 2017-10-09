565d3a429d87e49b06d074fb95062361d21f61ae
Victor Peralta <peralta.r.victor@gmail.com>
2017-10-08 20:02:44 -0700

    Created statistics.py for tabulation and graphs

---
+++ statistics.py
@@ -0,0 +1,11 @@
+import tabulate
+
+def printTable(orderList):
+    orders = []
+    for order in orderList:
+        quantity = 0
+        for suborder in order.subOrdenes:
+            quantity += suborder.quantity
+        orders.append([order.request_id, quantity])
+
+    print(tabulate.tabulate(orders, headers=["Request ID", "Quantity"]))
