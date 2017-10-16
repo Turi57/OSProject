b222ebe0dbb0b50be75c0491ac6b834a27618bcf
Fernando Raul Cortez Chavez <fernandoraulcc@gmail.com>
2017-10-16 16:34:36 -0700

    Add graphBars() prototype

--- statistics.py
+++ statistics.py
@@ -9,3 +9,6 @@ def printTable(orderList):
         orders.append([order["request_id"], quantity])
 
     print(tabulate.tabulate(orders, headers=["Request ID", "Quantity"]))
+
+def graphBars(listaEtiquetas, listaValores):
+    ### Here is the function to graph a list of values with a list of labels
