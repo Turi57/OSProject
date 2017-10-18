867abd926710c1737c1621db61b670bbd0ab2860
Fernando Raul Cortez Chavez <fernandoraulcc@gmail.com>
2017-10-18 16:16:08 -0700

    Draft1 of first turn over of program

--- tacosMain.py
+++ tacosMain.py
@@ -4,13 +4,18 @@ from statistics import *
 
 ##### MAIN #####
 
-# Read from SQS and save all orders to orders.json
-saveOrder();
+# Read from SQS and store the json strings in a list
+listaOrdenes = readSQS();
 
 # Read orders.json and create a list with all orders as json objects (dictionaries)
-listaOrdenes = readOrdersFile()
+#listaOrdenes = readOrdersFile()
 
 # Print a table of the current orders and their details
 printTable(listaOrdenes)
 
-# (UNDER DEVELOPMENT) Graph different aspects of the current orders
+# Graph different aspects of the current orders
+tipos = obtainTacosByType(listaOrdenes)
+carnes = obtainTacosByMeat(listaOrdenes)
+
+graphTacosByType(tipos)
+graphTacosByMeat(carnes)
