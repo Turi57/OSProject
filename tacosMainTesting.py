2d27417dee54fac07b9330d45af9b1b6c5d797d2
fernandoraulcortezchavez <fernandoraulcc@gmail.com>
2017-11-13 20:26:38 -0800

    Moving assist files to extra

---
+++ tacosMainTesting.py
@@ -0,0 +1,24 @@
+from jsonOrder import *
+from saveOrder import *
+from statistics import *
+
+##### MAIN #####
+
+# Read from SQS and store the json strings in a list
+#listaOrdenes = readSQS();
+
+# Read orders.json and create a list with all orders as json objects (dictionaries)
+listaOrdenes = readOrdersFile()
+
+# Print a table of the current orders and their details
+printTable(listaOrdenes)
+
+# Graph different aspects of the current orders
+tipos = obtainTacosByType(listaOrdenes)
+carnes = obtainTacosByMeat(listaOrdenes)
+
+graphTacos(tipos, "Tipos")
+graphTacos(carnes, "Carnes")
+
+#Wait to see the graphs
+input("")
