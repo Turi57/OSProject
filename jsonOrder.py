f68e7878f36ba8d720fba196720468a1cd962dc9
Victor Peralta <peralta.r.victor@gmail.com>
2017-11-20 15:31:42 -0800

    Put doc comments inside functions for jsonOrder.py

--- jsonOrder.py
+++ jsonOrder.py
@@ -19,17 +19,16 @@ def readOrdersFile():
                 break
         return listaOrdenes
     
-"""Function that receives the string from SQS parsed to JSON object, and will add the answer's attributes """
 def initializeResponse(inputJSON):
+    """Function that receives the string from SQS parsed to JSON object, and will add the answer's attributes """
     pass
 
-"""Function that receives a response JSON object, a string that describes the step´s state and
-another one that describes the action done."""
 def addStepToAnswer(responseJSON, stateString, actionString):
+    """Function that receives a response JSON object, a string that describes the step´s state and
+    another one that describes the action done."""
     pass
 
-"""Function that sets the start and end attributes of the response JSON object to specified parameters"""
 def setOrderTimes(responseJOSN, start, end):
+    """Function that sets the start and end attributes of the response JSON object to specified parameters"""
     pass
-    
 
